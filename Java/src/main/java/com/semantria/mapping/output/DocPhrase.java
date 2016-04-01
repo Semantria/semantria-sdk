4d5dacaaa83813503ad4a47eed6c2a8b9dd1364b
Heorhiy Kozlov <georgek@Georges-Mac-Pro.local>
2016-04-02 00:21:30 +0300

    Added missed is_intensified and intensifying_phrase fields to Java and strong-typed .Net SDK

--- Java/src/main/java/com/semantria/mapping/output/DocPhrase.java
+++ Java/src/main/java/com/semantria/mapping/output/DocPhrase.java
@@ -6,8 +6,10 @@ public class DocPhrase
 {
     private String title = null;
     private Float sentiment_score = null;
-    private String negating_phrase = null;
     private Boolean is_negated = null;
+    private String negating_phrase = null;
+    private Boolean is_intensified = null;
+    private String intensifying_phrase = null;
 	private String sentiment_polarity = null;
 	private String type = null;
 
@@ -15,10 +17,14 @@ public class DocPhrase
     public String getTitle() { return title; }
     @XmlElement(name="sentiment_score")
     public Float getSentimentScore() { return sentiment_score; }
-    @XmlElement(name="negating_phrase")
-    public String getNegatingPhrase() { return negating_phrase; }
     @XmlElement(name="is_negated")
     public Boolean getIsNegated() { return is_negated; }
+    @XmlElement(name="negating_phrase")
+    public String getNegatingPhrase() { return negating_phrase; }
+    @XmlElement(name="is_intensified")
+    public Boolean getIsIntensified() { return is_intensified; }
+    @XmlElement(name="intensifying_phrase")
+    public String getIntensifyingPhrase() { return intensifying_phrase; }
 	@XmlElement(name = "sentiment_polarity")
 	public String getSentimentPolarity() { return sentiment_polarity; }
 	@XmlElement(name = "type")
@@ -28,6 +34,8 @@ public class DocPhrase
     public void setSentimentScore(Float sentiment_score) {this.sentiment_score = sentiment_score;}
     public void setNegatingPhrase(String negating_phrase) {this.negating_phrase = negating_phrase;}
     public void setIsNegated(Boolean is_negated) {this.is_negated = is_negated;}
+    public void setIsIntensified(Boolean is_intensified) {this.is_intensified = is_negated;}
+    public void setIntensifyingPhrase(String intensifying_phrase) {this.intensifying_phrase = negating_phrase;}
 	public void setSentimentPolarity(String sentiment_polarity) { this.sentiment_polarity = sentiment_polarity; }
 	public void setType(String type) { this.type = type; }
 }
--- dotNet/Semantria.Com/Mapping/Output/DocPhrase.cs
+++ dotNet/Semantria.Com/Mapping/Output/DocPhrase.cs
@@ -9,13 +9,29 @@ namespace Semantria.Com.Mapping.Output
     [XmlRootAttribute("phrase", Namespace = "")]
     public class DocPhrase
     {
-        [DataMember(Name = "negating_phrase")]
-        [XmlElementAttribute("negating_phrase")]
-        public string NegatingPhrase { get; set; }
+        [DataMember(Name = "title")]
+        [XmlElementAttribute("title")]
+        public string Title { get; set; }
+
+        [DataMember(Name = "type")]
+        [XmlElementAttribute("type")]
+        public string Type { get; set; }
 
         [DataMember(Name = "is_negated")]
         [XmlElementAttribute("is_negated")]
         public bool IsNegated { get; set; }
+
+        [DataMember(Name = "negating_phrase")]
+        [XmlElementAttribute("negating_phrase")]
+        public string NegatingPhrase { get; set; }
+
+        [DataMember(Name = "is_intensified")]
+        [XmlElementAttribute("is_intensified")]
+        public bool IsIntensified { get; set; }
+
+        [DataMember(Name = "intensifying_phrase")]
+        [XmlElementAttribute("intensifying_phrase")]
+        public string IntensifyingPhrase { get; set; }
 
         [DataMember(Name = "sentiment_score")]
         [XmlElementAttribute("sentiment_score")]
@@ -24,13 +40,5 @@ namespace Semantria.Com.Mapping.Output
         [DataMember(Name = "sentiment_polarity")]
         [XmlElementAttribute("sentiment_polarity")]
         public string SentimentPolarity { get; set; }
-
-        [DataMember(Name = "title")]
-        [XmlElementAttribute("title")]
-        public string Title { get; set; }
-
-        [DataMember(Name = "type")]
-        [XmlElementAttribute("type")]
-        public string Type { get; set; }
     }
 }
